5034d95fedf102c5b207b343db5eaf3fcabc0cdb
Scott Gemmell <Scott_Gemmell@syntelinc.com>
2019-03-27 20:12:54 +0000

    typescript

--- src/ducks/news.ts
+++ src/ducks/news.ts
@@ -1,7 +1,7 @@
-
 /*
  * Types
  */
+
 export const types = {
 	NEWS: "[News]",
 	GET_NEWS: "[News] GET_NEWS",
@@ -11,14 +11,32 @@ export const types = {
 /*
  * Actions
  */
-const getNewsRequest = (query:string) => {
+
+export type NewsAction = 
+| { type: string; query: string }
+| { type: string; news: NewsItem[]; meta: { feature: string; normalizeKey: string} };
+
+export interface NewsItem {
+	id: number
+	title: string
+	url: string
+	created: string
+}
+
+export interface NewsSuccess {
+	news: NewsItem[]
+	feature: string
+	normalizeKey: string
+}
+
+const getNewsRequest = (query:string):NewsAction => {
 	return {
 		type: types.GET_NEWS,
 		query,
 	}
 }
 
-const getNewsSuccess = ({ news, feature, normalizeKey }:any) => {
+const getNewsSuccess = ({ news, feature, normalizeKey }:NewsSuccess):NewsAction => {
 	return {
 		type: types.SET_NEWS,
 		news,
@@ -37,9 +55,14 @@ export const actions = {
 /*
  * Reducer
  */
-const initialState:any = [];
+// export interface News {
+// 	news: []
+// 	action:any
+// }
 
-export const newsReducer = (news = initialState, action:any) => {
+const initialState:Array<NewsItem> = []
+
+export const newsReducer = (news = initialState, action:any):any => {
 	switch(action.type) {
 		case types.SET_NEWS:
 			return action.news;
