088b8ee6b666245ae1bc4ac091be72fb98ecba49
Scott Gemmell <Scott_Gemmell@syntelinc.com>
2019-03-28 10:34:10 +0000

    typescript

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,44 +1,41 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { NewsItem } from "../ducks/news";
 import * as R from "ramda";
 
 interface SearchResultsProps {
-	news: any;
+	news: NewsItem[];
 }
 
 class SearchResults extends Component<SearchResultsProps> {
 
-  render(){
+	render(){
 
-    const { news } = this.props;
-    //console.log('articles', articles)
+		const { news } = this.props;
+		//console.log('articles', articles)
 
-    if(!news || R.isEmpty(news)) {
-      return null;
-    }
+		if(!news || R.isEmpty(news)) {
+			return null;
+		}
 
-
-    return (
-      <div>
-        <div>
-            <h2>
-              Results
-            </h2>
-            <ul>
-            {news.map((article:any) => {
-              return (
-                <li key={article.id}>
-                    <a href={article.url}>{article.title}</a>
-                    <br/>
-                    <small>{article.created}</small>
-                </li> 
-              )
-            })}
-              
-            </ul>
-          </div>
-    </div>
-  )
-  }
+		return (
+			<div>
+				<h2>
+					Results
+				</h2>
+				<ul>
+					{news.map((article:NewsItem) => {
+					return (
+						<li key={article.id}>
+							<a href={article.url}>{article.title}</a>
+							<br/>
+							<small>{article.created}</small>
+						</li> 
+					)
+					})}      
+				</ul>
+			</div>
+		)
+	}
 }
 
 export default SearchResults;
