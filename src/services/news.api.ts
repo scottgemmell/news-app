24f4bcd5160bad8cfb5df7a55e03e5473511a6d3
Scott Gemmell <scott.gemmell@mac.com>
2019-04-01 15:47:50 +0100

    remove default

--- src/services/news.api.ts
+++ src/services/news.api.ts
@@ -2,8 +2,7 @@
 import { create } from "apisauce";
 
 const api = create({
-	baseURL: "http://hn.algolia.com/api/v1",
-	headers: { Accept: "application/json" }
+	baseURL: "http://hn.algolia.com/api/v1"
 });
 
 export const getNewsApiRequest = (query:string) => {
