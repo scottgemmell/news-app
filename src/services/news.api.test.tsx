4573e78c95c6a5f41c43c28a15f4e80a3e6bf6c7
Scott Gemmell <scott.gemmell@mac.com>
2019-04-01 16:17:07 +0100

    rename

--- src/services/news.api.test.tsx
+++ src/services/news.api.test.tsx
@@ -1,5 +1,5 @@
 import { getNewsApiRequest } from "./news.api";
 
-it("test", () => {
+it("Fetch API (promise)", () => {
 	expect(getNewsApiRequest("dogs")).resolves.toEqual("http://hn.algolia.com/api/v1/search?query=dogs");
 });
